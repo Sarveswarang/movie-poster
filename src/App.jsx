190c2ad859c12e3594d88f72354dd514f0097414
Sarveswarang <2611sarveswaran@gmail.com>
2023-02-02 20:01:10 +0530

    delete,useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from '@mui/material/Button';
@@ -28,104 +28,16 @@ import { dark } from '@mui/material/styles/createPalette';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import {Addmovie}from"./Add movie";
+
 // import { useNavigate } from 'react-router-dom';
 // import{double} from './welcome'
 
 // console.log(double(4))
-const trailer=[
-  {
-    "id": "99",
-    "name": "Vikram",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-    "rating": 8.4,
-    "summary": "Members of a black ops team must track and eliminate a gang of masked murderers.",
-    "trailer": "https://www.youtube.com/embed/OKBMCL-frPU"
-  },
-  {
-    "id": "100",
-    "name": "RRR",
-    "poster": "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-    "rating": 8.8,
-    "summary": "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
-    "trailer": "https://www.youtube.com/embed/f_vbAtFSEc0"
-  },
-  {
-    "id": "101",
-    "name": "Iron man 2",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-    "rating": 7,
-    "summary": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-    "trailer": "https://www.youtube.com/embed/wKtcmiifycU"
-  },
-  {
-    "id": "102",
-    "name": "No Country for Old Men",
-    "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    "rating": 8.1,
-    "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    "trailer": "https://www.youtube.com/embed/38A__WT3-o0"
-  },
-  {
-    "id": "103",
-    "name": "Jai Bhim",
-    "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-    "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-    "rating": 8.8,
-    "trailer": "https://www.youtube.com/embed/nnXpbTFrqXA"
-  },
-  {
-    "id": "104",
-    "name": "The Avengers",
-    "rating": 8,
-    "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8"
-  },
-  {
-    "id": "105",
-    "name": "Interstellar",
-    "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-    "rating": 8.6,
-    "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    "trailer": "https://www.youtube.com/embed/zSWdZVtXT7E"
-  },
-  {
-    "id": "106",
-    "name": "Baahubali",
-    "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-    "rating": 8,
-    "summary": "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    "trailer": "https://www.youtube.com/embed/sOEg_YZQsTI"
-  },
-  {
-    "id": "107",
-    "name": "Ratatouille",
-    "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-    "rating": 8,
-    "summary": "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    "trailer": "https://www.youtube.com/embed/NgsQ8mVkN8w"
-  },
-  {
-    "name": "PS2",
-    "poster": "https://m.media-amazon.com/images/M/MV5BYjFjMTQzY2EtZjQ5MC00NGUyLWJiYWMtZDI3MTQ1MGU4OGY2XkEyXkFqcGdeQXVyNDExMjcyMzA@._V1_.jpg",
-    "summary": "Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan",
-    "rating": 8,
-    "trailer": "https://www.youtube.com/embed/KsH2LA8pCjo",
-    "id": "108"
-  },
-  {
-    "name": "Thor: Ragnarok",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
-    "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
-    "rating": 8.8,
-    "trailer": "https://youtu.be/NgsQ8mVkN8w",
-    "id": "109"
-  }
-]
+
  
 
 function App() {
-  const [movieflow,setmovieflow] =useState( trailer);
+  const [movieflow,setmovieflow] =useState( []);
   const navigate=useNavigate();
   const [mode,setmode] =useState("light");
   const darkTheme = createTheme({
@@ -136,7 +48,12 @@ function App() {
   const bgstyle={
 borderRadius:"0px",
 minHeight:"100vh",
-  }
+  };
+  useEffect(()=>{
+    fetch("https://63db579fb8e69785e47fc741.mockapi.io/movie")
+    .then((data)=>data.json())
+    .then((mvs)=>setmovieflow(mvs))
+},[]);
   
   return ( 
    
@@ -162,9 +79,9 @@ minHeight:"100vh",
      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Movielist" element={<Movielist movieflow={movieflow} setmovieflow={setmovieflow}/>} />
+        <Route path="/Movielist" element={<Movielist />} />
         <Route path="/flimlist" element={ <Navigate replace to ="/Movielist"/>} />
-        <Route path="/Movielist/:id" element={<Mdetails movieflow={movieflow}/>} />
+        <Route path="/Movielist/:id" element={<Mdetails />} />
         <Route path="/color game" element={<Color/>} />
         <Route path="/Add movie" element={<Addmovie movieflow={movieflow} setmovieflow={setmovieflow}/>} />
 
@@ -176,9 +93,16 @@ minHeight:"100vh",
 
     </ThemeProvider>
   );
-  function Mdetails({movieflow}){
-        const {id} = useParams();
-        const movie=movieflow[id];
+  function Mdetails(){
+    const [movie,setmovie] =useState({});
+    const {id} = useParams();
+
+    useEffect(()=>{
+      fetch(`https://63db579fb8e69785e47fc741.mockapi.io/movie/${id}`)
+      .then((data)=>data.json())
+      .then((mvs)=>setmovie(mvs))
+  },[id]);
+        // const movie=movieflow[id];
         const style = {
           color: movie.rating > 8 ? "green" : "crimson",
         };
@@ -228,22 +152,30 @@ minHeight:"100vh",
     );
   }
  
-  function Movielist({movieflow,setmovieflow}) {
+  function Movielist() {
+    const [movieflow,setmovieflow] =useState( []);
+const getmovies = ()=>{
+  fetch("https://63db579fb8e69785e47fc741.mockapi.io/movie",{method: "GET"})
+  .then((data)=>data.json())
+  .then((mvs)=>setmovieflow(mvs))
+};
+    useEffect(()=>getmovies(),[]);
+  const deletemovie=(id)=>{
+    fetch(`https://63db579fb8e69785e47fc741.mockapi.io/movie/${id}`,{method: "DELETE"})
+    .then(()=>getmovies());
 
-    fetch("https://63db579fb8e69785e47fc741.mockapi.io/movie")
-    .then((data)=>data.json())
-    .then((mvs)=>console.log(mvs))
-
+  };
     return ( 
       <div>
       
       <div className='movie-css'>
-        {movieflow.map((nm, index) => (<Movie key={index} movie={nm} id={index}/>))}
+        {movieflow.map((nm) => (<Movie key={nm.id} movie={nm} id={nm.id} 
+        deletebutton={<button onClick={()=>deletemovie(nm.id)}>delete</button>}/>))}
       </div>
       </div>
     );
   }
-  function Movie({ movie,id }) {
+  function Movie({ movie,id ,deletebutton}) {
     const [show, setshow] = useState(false)
     const disstyle = {
       display: show ? "none" : "block",
@@ -271,7 +203,7 @@ minHeight:"100vh",
         <p style={disstyle}>{movie.summary}</p>
         </CardContent>
         <CardActions>
-        <Like />
+        <Like /> {deletebutton}
         </CardActions>
         
       
